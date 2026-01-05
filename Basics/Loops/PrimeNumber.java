d62dee41fd58dafb124fcd7512a6d4f8e17fcaaf
Mayank0204 <mayanksangwan20022004@gmail.com>
2026-01-05 23:57:27 +0530

    add Java code for PrimeNumber

---
+++ Basics/Loops/PrimeNumber.java
@@ -0,0 +1,12 @@
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num = 29;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                System.out.println(num + " is not a prime number.");
+                return;
+            }
+        }
+        System.out.println(num + " is a prime number.");
+    }
+}
