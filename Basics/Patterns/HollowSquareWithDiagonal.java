1335b599fd95274cd7cdf624b274301d3fdfdbf3
Mayank0204 <mayanksangwan20022004@gmail.com>
2026-01-26 23:59:54 +0530

    add Java code for Hollow Square with Diagonal pattern

---
+++ Basics/Patterns/HollowSquareWithDiagonal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HollowSquareWithDiagonal {  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                if (row == 1 || col == 1 || row == n || col == n || row == col || col == n - row + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+        sc.close();
+    }
+
+}
