affb7d48e30e011ab41a1b6ea9803eea14f1ddcf
Mayank0204 <mayanksangwan20022004@gmail.com>
2026-02-01 01:35:21 +0530

    add Java code for Left Inverted Hollow Triangle pattern

---
+++ Basics/Patterns/LeftInvertedHollowTriangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LeftInvertedHollowTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n - row + 1; col++) {
+                if (row == 1 || col == 1 || col == n - row + 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
